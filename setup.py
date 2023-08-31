7e9c55adb993fd1aa5126251167f8bd4b54579ec
Vinicius Mesel <4984147+vmesel@users.noreply.github.com>
2023-08-31 11:13:37 -0600

    Bumps tap requests version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Singer.io tap for extracting data from the Microsoft Dynamics 365 API",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     install_requires=[
-        "requests==2.22.0",
+        "requests==2.29.0",
         "singer-python==5.8.1",
         "odata @ https://github.com/dreamdata-io/python-odata/archive/master.zip",
     ],
