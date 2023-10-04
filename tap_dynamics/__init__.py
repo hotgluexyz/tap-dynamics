2c7fe29765a4749500f43e3f09693496093ca123
Joshua Gross <josh@goodkind.com>
2023-10-04 15:37:53 -0400

    Allow alternate authentication paramaters * allow grant type to be defined or default to "refresh_token" * allow username and password authentication * allow alternate auth url

--- tap_dynamics/__init__.py
+++ tap_dynamics/__init__.py
@@ -36,7 +36,7 @@ def do_discover(service, get_lookup_tables):
 
 
 class DynamicsAuth(requests.auth.AuthBase):
-    def __init__(self, parsed_args, url):
+    def __init__(self, parsed_args, url, auth_url):
         self.__config = parsed_args.config
         self.__config_path = parsed_args.config_path
         self.__resource = url
@@ -45,22 +45,35 @@ class DynamicsAuth(requests.auth.AuthBase):
         self.__redirect_uri = parsed_args.config["redirect_uri"]
         self.__refresh_token = parsed_args.config["refresh_token"]
 
+        self.__auth_url = auth_url
+        self.__grant_type = parsed_args.config["grant_type"] or "refresh_token"
+        self.__username = parsed_args.config["username"]
+        self.__password = parsed_args.config["password"]
+
         self.__session = requests.Session()
         self.__access_token = None
         self.__expires_at = None
 
     def ensure_access_token(self):
         if self.__access_token is None or self.__expires_at <= datetime.utcnow():
-            response = self.__session.post(
-                "https://login.microsoftonline.com/common/oauth2/token",
-                data={
+
+            data = {
                     "client_id": self.__client_id,
                     "client_secret": self.__client_secret,
                     "redirect_uri": self.__redirect_uri,
-                    "refresh_token": self.__refresh_token,
-                    "grant_type": "refresh_token",
+                    "grant_type": self.__grant_type,
                     "resource": self.__resource,
-                },
+                }
+            if self.__grant_type == "refresh_token":
+                data["refresh_token"] = self.__refresh_token
+            
+            if self.__password and self.__username:
+                data["password"] = self.__password
+                data["username"] = self.__username
+
+            response = self.__session.post(
+                self.__auth_url,
+                data=data,
             )
 
             if response.status_code != 200:
@@ -94,15 +107,28 @@ def main():
         url = parsed_args.config['full_url']
     else:
         url = "https://{}.crm.dynamics.com".format(parsed_args.config["org"])
-    auth = DynamicsAuth(parsed_args, url)
+
+    if parsed_args.config.get('auth_url'):
+        auth_url = parsed_args.config['auth_url']
+    else: 
+        auth_url = "https://login.microsoftonline.com/common/oauth2/token"
+
+
+    auth = DynamicsAuth(parsed_args, url, auth_url)
     session = requests.Session()
     session.headers.update({"Prefer": 'odata.include-annotations="*"'})
-    service_url = url + "/api/data/v9.0/"
+
+    if  "/api/data/" not in url:
+        service_url = url + "/api/data/v9.0/"
+    else:
+        service_url = url
+
     service = ODataService(
         service_url,
         reflect_entities=True,
         auth=auth,
-        session = session
+        session=session,
+        quiet_progress=True,
     )
     get_lookup_tables = parsed_args.config.get("get_lookup_tables", False)
     catalog = parsed_args.catalog or do_discover(service, get_lookup_tables)
